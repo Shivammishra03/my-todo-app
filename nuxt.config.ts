12c247ca420c74fbf3faf7200c3a43cbe085e147
Shivam Mishra <shivammishra221310@gmail.com>
2024-10-19 20:37:31 +0530

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+
   modules: [
     '@nuxt/ui',
     '@pinia/nuxt',
   ],
+
   pinia: {
     autoImports: [
       // automatically imports `defineStore`
@@ -12,13 +14,18 @@ export default defineNuxtConfig({
       ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
     ],
   },
+
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~/assets/css/main.css"
   ],
+
   imports: {
     dirs: ["./stores"],
-  }
+  },
+
+  compatibilityDate: "2024-10-19"
 })
